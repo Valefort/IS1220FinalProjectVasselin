bc72ed526d71bab6ebafb608a5740c30858486bd
Fomage <Fomage@138.195.145.195>
2015-03-28 16:59:49 +0100

    first version of the interface VItem (javadoc included) and VItemNotFoundException.

---
+++ src/fr/ecp/IS1220/FinalProject/Vasselin/core/VItem.java
@@ -0,0 +1,39 @@
+package fr.ecp.IS1220.FinalProject.Vasselin.core;
+
+import java.util.List;
+
+public interface VItem {
+	
+	/**
+	 * @return the list of the successors of this.
+	 */
+	public List<VItem> getSuccessors();
+	
+	/**
+	 * @param adds the VItem i to the successors of this.
+	 */
+	public void add(VItem i);
+	
+	/**
+	 * @param s is the direct successor of this that will be removed and returned.
+	 * @return s is the direct successor of this that was removed.
+	 * May throw a VItemNotFoundException if s is not a direct successor of this.
+	 */
+	public VItem remove(VItem s) throws VItemNotFoundException;
+	
+	/**
+	 * @param i - the VItem we want to find in the direct successors of this.
+	 * @return true if i is among the direct successors of this, false otherwise.
+	 */
+	public boolean contains(VItem i);
+	
+	/**
+	 * @return the size of this, in bytes.
+	 */
+	public long getSize();
+	
+	/**
+	 * @return the name of this (which is used in the pathes internal to the VSF).
+	 */
+	public String getName();
+}
---
+++ src/fr/ecp/IS1220/FinalProject/Vasselin/core/VItemNotFoundException.java
@@ -0,0 +1,7 @@
+package fr.ecp.IS1220.FinalProject.Vasselin.core;
+
+public class VItemNotFoundException extends Exception {
+	
+	private static final long serialVersionUID = -1762449495377406717L;
+
+}
