5c8a53c107b6a98b18f260a32b042572a42d188b
Fomage <Fomage@138.195.145.195>
2015-03-28 17:29:01 +0100

    VFile and VDirectory added. NOT implemented yet.

---
+++ src/fr/ecp/IS1220/FinalProject/Vasselin/core/VFile.java
@@ -0,0 +1,44 @@
+package fr.ecp.IS1220.FinalProject.Vasselin.core;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class VFile implements Serializable, VItem {
+
+	@Override
+	public List<VItem> getSuccessors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(VItem i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public VItem remove(VItem s) throws VItemNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(VItem i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
