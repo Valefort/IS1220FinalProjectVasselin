aee56cf6bff215ac1fcc73a09a7b9cf796f95cf1
Fomage <Fomage@138.195.145.195>
2015-03-31 21:56:37 +0200

    Added VItemFactory, to manage importation of VItems. Not implemented yet.

---
+++ src/fr/ecp/IS1220/FinalProject/Vasselin/core/VItemFactory.java
@@ -0,0 +1,17 @@
+package fr.ecp.IS1220.FinalProject.Vasselin.core;
+
+import java.nio.file.Path;
+
+public class VItemFactory {
+
+	/**
+	 * Creates a VItem from the file/directory targeted by path.
+	 * Note : internally uses the importVItem from VDirectory/VFile
+	 * @param path : the path from which to import the VItem
+	 * @return a VItem created from the target of the path.
+	 */
+	public static VItem importVItem(Path path){
+		return null;
+	}
+	
+}
